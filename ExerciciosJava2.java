ecb2d1f37007a6edc6472bca29896ef1551781b8
Bruno Nunes <139359503+bruno08nunes@users.noreply.github.com>
2026-03-11 21:52:09 -0300

    Add files via upload

---
+++ ExerciciosJava2.java
@@ -0,0 +1,144 @@
+import java.util.Scanner;
+
+public class ExerciciosJava2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // EXERCÍCIO 1 - OPERAÇÕES COM DOIS NÚMEROS
+
+        // double n1 = sc.nextDouble();
+        // double n2 = sc.nextDouble();
+
+        // System.out.println("Soma: " + (n1 + n2));
+        // System.out.println("Subtração: " + (n1 - n2));
+        // System.out.println("Multiplicação: " + (n1 * n2));
+        // System.out.println("Divisão real: " + (n1 / n2));
+        // System.out.println("Divisão inteira: " + ((int)n1 / (int)n2));
+        // System.out.println("Resto da divisão: " + ((int)n1 % (int)n2));
+        // System.out.println("Potência: " + Math.pow(n1, n2));
+        // System.out.println("Maior: " + Math.max(n1, n2));
+        // System.out.println("Menor: " + Math.min(n1, n2));
+
+        // EXERCÍCIO 2 - CELSIUS PARA FAHRENHEIT
+
+        // System.out.print("Digite a temperatura em Celsius: ");
+        // double c = sc.nextDouble();
+
+        // double f = c * 1.8 + 32;
+
+        // System.out.println("Temperatura em Fahrenheit: " + f);
+
+        // EXERCÍCIO 3 - FAHRENHEIT PARA CELSIUS
+
+        // System.out.print("Digite a temperatura em Fahrenheit: ");
+        // double f2 = sc.nextDouble();
+
+        // double c2 = (f2 - 32) / 1.8;
+
+        // System.out.println("Temperatura em Celsius: " + c2);
+
+        // EXERCÍCIO 4 - KM/H PARA M/S
+
+        // System.out.print("Digite a velocidade em km/h: ");
+        // double kmh = sc.nextDouble();
+
+        // double ms = kmh / 3.6;
+
+        // System.out.println("Velocidade em m/s: " + ms);
+
+        // EXERCÍCIO 5 - DESCONTO EM CALÇADO
+
+        // System.out.print("Digite o preço do calçado: ");
+        // double preco = sc.nextDouble();
+
+        // System.out.print("Digite o percentual de desconto: ");
+        // double desconto = sc.nextDouble();
+
+        // double valorDesconto = preco * (desconto / 100);
+        // double valorFinal = preco - valorDesconto;
+
+        // System.out.println("Valor do desconto: " + valorDesconto);
+        // System.out.println("Valor final a pagar: " + valorFinal);
+
+        // EXERCÍCIO 6 - CÍRCULO
+
+        // System.out.print("Digite o raio do círculo: ");
+        // double raio = sc.nextDouble();
+
+        // double diametro = 2 * raio;
+        // double circunferencia = 2 * Math.PI * raio;
+        // double areaCirculo = Math.PI * raio * raio;
+
+        // System.out.println("Diâmetro: " + diametro);
+        // System.out.println("Circunferência: " + circunferencia);
+        // System.out.println("Área: " + areaCirculo);
+
+        // EXERCÍCIO 7 - RETÂNGULO
+
+        // System.out.print("Digite a largura do retângulo: ");
+        // double largura = sc.nextDouble();
+
+        // System.out.print("Digite a altura do retângulo: ");
+        // double altura = sc.nextDouble();
+
+        // double perimetroRet = 2 * (largura + altura);
+        // double areaRet = largura * altura;
+
+        // System.out.println("Perímetro: " + perimetroRet);
+        // System.out.println("Área: " + areaRet);
+
+        // EXERCÍCIO 8 - TRIÂNGULO (ÁREA)
+
+        // System.out.print("Digite a base do triângulo: ");
+        // double baseTri = sc.nextDouble();
+
+        // System.out.print("Digite a altura do triângulo: ");
+        // double alturaTri = sc.nextDouble();
+
+        // double areaTri = (baseTri * alturaTri) / 2;
+
+        // System.out.println("Área do triângulo: " + areaTri);
+
+        // EXERCÍCIO 9 - POTÊNCIA
+
+        // System.out.print("Digite o primeiro número: ");
+        // double num1 = sc.nextDouble();
+
+        // System.out.print("Digite o segundo número: ");
+        // double num2 = sc.nextDouble();
+
+        // double potencia = Math.pow(num1, num2);
+
+        // System.out.println("Resultado da potência: " + potencia);
+
+        // EXERCÍCIO 10 - TRIÂNGULO RETÂNGULO
+
+        // System.out.print("Digite o primeiro cateto: ");
+        // double cateto1 = sc.nextDouble();
+
+        // System.out.print("Digite o segundo cateto: ");
+        // double cateto2 = sc.nextDouble();
+
+        // double hipotenusa = Math.sqrt((cateto1 * cateto1) + (cateto2 * cateto2));
+
+        // double perimetroTri = cateto1 + cateto2 + hipotenusa;
+
+        // double areaTriRet = (cateto1 * cateto2) / 2;
+
+        // double seno = cateto1 / hipotenusa;
+        // double cosseno = cateto2 / hipotenusa;
+        // double tangente = cateto1 / cateto2;
+
+        // System.out.println("Hipotenusa: " + hipotenusa);
+        // System.out.println("Perímetro: " + perimetroTri);
+        // System.out.println("Área: " + areaTriRet);
+        // System.out.println("Seno: " + seno);
+        // System.out.println("Cosseno: " + cosseno);
+        // System.out.println("Tangente: " + tangente);
+
+
+        sc.close();
+    }
+}
